ec32cbb74665307589a50cde34dd39ace841e9f4
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-06-12 11:16:20 -0700

    R-ALAB 308H.13.1 - DOM Manipulation (Part Two)

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 // Menu data structure
 var menuLinks = [
   {text: 'about', href: '/about'},
-  {text: 'catalog', href: '/catalog'},
-  {text: 'orders', href: '/orders'},
-  {text: 'account', href: '/account'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
 // 1. Select and cache the <main> element in a variable named mainEl.
@@ -37,12 +48,93 @@ console.log(topMenuEl)
 //10. Iterate over the entire menuLinks array and for each "link" object:
 
 menuLinks.forEach((ele) => {
-  //11. Create an <a> element.
+//11. Create an <a> element.
   const anchor = document.createElement('a');
-  //12. On the new element, add an href attribute with its value set to the href property of the "link" object.
+//12. On the new element, add an href attribute with its value set to the href property of the "link" object.
   anchor.href = ele.href;
-  //13. Set the new element's content to the value of the text property of the "link" object.
+//13. Set the new element's content to the value of the text property of the "link" object.
   anchor.textContent = ele.text;
-  //14. Append the new element to the topMenuEl element.
+//14. Append the new element to the topMenuEl element.
   topMenuEl.appendChild(anchor)
 })
+
+// R_ALAB 308H.13.1 - DOM Manuipulation (Part Two)
+
+//1.  Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl. 
+const subMenuEl = document.querySelectorAll('#sub-menu')
+
+//2. Set the height subMenuEl element to be 100%.
+subMenuEl[0].style.height = '100%';
+
+//3. Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl[0].style.backgroundColor = 'var(--sub-menu-bg)'
+
+//4. Add the class of flex-around to the subMenuEl element.
+subMenuEl[0].classList.add('flex-around')
+
+//5. Set the CSS position property of subMenuEl to the value of absolute.
+subMenuEl[0].style.position = 'absolute'
+
+//6. Set the CSS top property of subMenuEl to the value of 0.
+subMenuEl[0].style.top = 0;
+
+//8. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+  const topMenuLinks = document.getElementById('top-menu').getElementsByTagName('a')
+console.log('Top', topMenuLinks)
+// 9. Declare a global showingSubMenu variable and initialize it to false;
+let showingSubMenu = false;
+
+let subLinks = [];
+//10. Attach a delegated 'click' event listener to topMenuEl.
+const helperFunction =  (e) => {
+  e.preventDefault();
+  
+  Array.from([...topMenuLinks]).forEach((ele,) => {
+    ele.classList.remove('active');
+
+  })
+
+  menuLinks.forEach((ele) => {
+    if(ele.subLinks) {
+      console.log('sub2', ele.subLinks,)
+      subLinks.push(ele.subLinks)
+    showingSubMenu = true;
+  } else {
+    showingSubMenu = false;
+  }
+  })
+  const buildSubMenu = (arr) => {
+    console.log('sub', subMenuEl)
+    let i = 0;
+    arr.forEach((ele) => {
+      const anchor = document.createElement('a')
+      console.log('anchor', ele[i])
+      anchor.href = ele[i].href;
+      anchor.textContent = ele[i].text;
+      document.querySelectorAll('#sub-menu').appendChild(anchor);
+      i++
+    })
+  }
+  buildSubMenu(subLinks)
+  if(!e.target.matches('A')) {
+    return ''
+  } else {
+    console.log(e.target.textContent)
+    e.target.classList.add('active') 
+    console.log(document.getElementById('top-menu').getElementsByTagName('a') )
+    showingSubMenu = false;
+    subMenuEl[0].style.top = 0;
+    return
+  }
+
+ 
+}
+  topMenuEl.addEventListener('click', helperFunction)
+
+
+
+  console.log('sub', subLinks)
+
+  console.log('menulinks', menuLinks)
+
+
